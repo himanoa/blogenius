ff062beab90168e97c6e6afc8f484e1ec99b241b
himanoa <matsunoappy@gmail.com>
2024-04-21 09:16:27 +0900

    Add support markdown header

--- src/markdown.rs
+++ src/markdown.rs
@@ -13,14 +13,14 @@ pub enum RenderError {
     FailedExtractHeader
 }
 
-pub fn render<T>(body: &str) -> Result<HtmlOutput>
+pub fn render<T>(body: &str) -> Result<(T, HtmlOutput)>
 where 
     T: serde::de::DeserializeOwned,
 {
     return run_with::<T>(SyntectAdapterBuilder::new().theme("base16-ocean.dark"), body)
 }
 
-fn run_with<T>(builder: SyntectAdapterBuilder, body: &str) -> Result<String>
+fn run_with<T>(builder: SyntectAdapterBuilder, body: &str) -> Result<(T, String)>
 where 
     T: serde::de::DeserializeOwned,
 {
@@ -33,7 +33,7 @@ where
     plugins.render.codefence_syntax_highlighter = Some(&adapter);
 
 
-    return Ok(markdown_to_html_with_plugins(body_without_headers, &options, &plugins));
+    return Ok((headers, markdown_to_html_with_plugins(body_without_headers, &options, &plugins)));
 }
 
 #[cfg(test)]
@@ -49,7 +49,7 @@ mod tests {
     fn parse_should_be_return_to_heading () {
         let html = render::<Empty>("---\nfoo: 12\n---\n## Hello");
 
-        assert_eq!(html.unwrap(), "<h2>Hello</h2>\n")
+        assert_eq!(html.unwrap().1, "<h2>Hello</h2>\n")
     }
 
     #[test]
@@ -67,6 +67,6 @@ mod tests {
         let html = render::<Empty>(codeblock);
 
         let actual = "<pre style=\"background-color:#2b303b;\"><code class=\"language-rs\"><span style=\"color:#b48ead;\">fn </span><span style=\"color:#8fa1b3;\">main</span><span style=\"color:#c0c5ce;\">() -&gt; String {\n</span><span style=\"color:#c0c5ce;\">    &quot;</span><span style=\"color:#a3be8c;\">fooo</span><span style=\"color:#c0c5ce;\">&quot;\n</span><span style=\"color:#c0c5ce;\">}\n</span></code></pre>\n";
-        assert_eq!(html.unwrap(), actual);
+        assert_eq!(html.unwrap().1, actual);
     }
 }
