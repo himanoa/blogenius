88b62da5d86f38f7ed3ef04473c3176bc7e2f3a9
himanoa <matsunoappy@gmail.com>
2024-04-22 04:14:11 +0900

    Implemented build  command

--- src/command/build.rs
+++ src/command/build.rs
@@ -1,6 +1,23 @@
-use anyhow::Error;
-use crate::config::Config;
+use crate::{
+    article::repository::{ArticleRepository, HaveArticleRepository},
+    config::Config,
+    renderer::Renderer,
+    theme::repository::HaveThemeRepository,
+};
+use anyhow::Result;
+use std::fs::write;
 
-pub fn build(config: Config) -> Result<Unit> {
-    todo!()
+pub fn build<T>(kernel: T, config: Config) -> Result<()>
+where
+    T: HaveArticleRepository + HaveThemeRepository,
+{
+    let articles = kernel.article_repository().list()?;
+
+    for article in articles {
+        println!("Start generate {} article", &*article.id);
+        let rendered = kernel.render(&config.theme, &*article.id, &config.dist_path)?;
+        write(rendered.dist_path, rendered.body)?;
+        println!("End generate {} article", *article.id);
+    }
+    Ok(())
 }
