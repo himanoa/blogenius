366f591b371e7992740645ee723e981099af3e02
himanoa <matsunoappy@gmail.com>
2024-04-21 15:23:43 +0900

    Add get template path methods

--- src/theme/entity.rs
+++ src/theme/entity.rs
@@ -13,4 +13,43 @@ impl Theme {
             theme_path: theme_path.into(),
         }
     }
+
+    pub fn article_index_template(&self) -> PathBuf {
+        self.theme_path.join("article_index.html.hbs")
+    }
+
+    pub fn article_template(&self) -> PathBuf {
+        self.theme_path.join("article.html.hbs")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::Theme;
+
+    #[test]
+    fn article_index_template_should_be_return_to_joined_path() {
+        let theme = Theme::new("foo", "fixtures/foo");
+        assert_eq!(
+            theme.article_index_template(),
+            PathBuf::new()
+                .join("fixtures")
+                .join("foo")
+                .join("article_index.html.hbs")
+        )
+    }
+
+    #[test]
+    fn article_template_should_be_return_to_joined_path() {
+        let theme = Theme::new("foo", "fixtures/foo");
+        assert_eq!(
+            theme.article_template(),
+            PathBuf::new()
+                .join("fixtures")
+                .join("foo")
+                .join("article.html.hbs")
+        )
+    }
 }
