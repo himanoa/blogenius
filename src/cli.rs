5d9d06ca183b3c523dab90b8e9135f9d9a2a1f50
himanoa <matsunoappy@gmail.com>
2024-04-22 01:15:43 +0900

    Implemented a Cli arguments

---
+++ src/cli.rs
@@ -0,0 +1,14 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command[version, about, long_about = None]]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command[subcommand]]
+    pub(crate) command: Commands
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Build
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use clap::Parser;
+
+use crate::cli::{Cli, Commands};
+
 pub mod article;
 pub mod markdown;
 pub mod renderer;
 pub mod theme;
+pub mod cli;
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Build => {
+            println!("exec build")
+        }
+    }
 }
